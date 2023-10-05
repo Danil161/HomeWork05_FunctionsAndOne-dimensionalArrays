041be1da8920985b364e09069cf5006ed207f424
Danil <danil.bazarov@inbox.ru>
2023-10-05 17:48:47 +0300

    add Example002

---
+++ Example002/Program.cs
@@ -0,0 +1,64 @@
+﻿// Напишите программу, которая находит сумму элементов с нечетными индексами в одномерном массиве целых чисел и выводит результат на экран.
+
+// Программа должна иметь метод SumOddElements, который принимает массив целых чисел и возвращает сумму элементов с нечетными индексами в массиве.
+
+// Программа должна иметь метод PrintArray, который выводит элементы массива на экран.
+
+using System;
+
+public class Answer
+{
+    public static int SumOddElements(int[] array)
+    {
+// Введите свое решение ниже
+    int summ = 0;
+    for (int i = 1; i < array.Length; i += 2) 
+        summ += array[i];
+
+    return summ;
+    }
+
+    public static void PrintArray(int[] array)
+    {
+         // Введите свое решение ниже
+        for (int i = 0; i < array.Length; i++)
+            Console.Write(array[i] + "	");
+
+        Console.WriteLine();
+
+    }
+
+
+// Не удаляйте и не меняйте метод Main! 
+    public static void Main(string[] args)
+    {
+        int[] array;
+        if (args.Length == 0)
+        {
+           // Здесь вы можете поменять значения для отправки кода на Выполнение
+            array = new int[] { 12, 64, 28, 93, 35, 47, 6, 72, 58, 21 };
+        }
+        else
+        {
+            string[] argStrings = args[0].Split(", ");
+            array = new int[argStrings.Length];
+            for (int i = 0; i < argStrings.Length; i++)
+            {
+                if (int.TryParse(argStrings[i], out int number))
+                {
+                    array[i] = number;
+                }
+                else
+                {
+                    Console.WriteLine($"Ошибка при парсинге аргумента {argStrings[i]}.");
+                    return;
+                }
+            }
+        }
+
+        // Не удаляйте строки ниже
+        PrintArray(array);
+        int sumOdd = SumOddElements(array);
+        Console.WriteLine($"Сумма нечетных элементов: {sumOdd}");
+    }
+}
